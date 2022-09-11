d98008f9d0a417a306a500869a00eb8404a701f1
BhavyaArora <bhavya16arora@gmail.com>
2022-09-12 00:13:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StudentDataRetrieve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //File.Create("StuData.txt");
+            FileStream fs = new FileStream("StuData.txt",FileMode.Open,FileAccess.Read);
+
+            StreamReader sr = new StreamReader(fs);
+
+            while (true)
+            {
+                string sd = sr.ReadLine();
+
+                if (sd==null)
+                {
+                    Console.WriteLine("No data...");
+                    break;
+                }
+
+                Console.WriteLine("Student name: " + sd);
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
